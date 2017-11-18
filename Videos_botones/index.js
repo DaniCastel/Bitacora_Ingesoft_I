94ac300050f4b3fe9e13d8fcd95348c790135feb
DaniCastel <ldcastellanosa@gmail.com>
2017-11-18 17:04:03 -0500

    La página de videos y botones demora un poco en cargar

---
+++ Videos_botones/index.js
@@ -0,0 +1,18 @@
+function change1() {
+    var video = document.getElementById('videoback');
+        video.src = "https://www.youtube.com/embed/a2GujJZfXpg?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W0LHTWG-UmQ";
+ 
+}
+
+function change2() {
+    var video = document.getElementById('videoback');
+        video.src = "https://www.youtube.com/embed/cZAw8qxn0ZE?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W0LHTWG-UmQ";
+ 
+}
+
+
+function change3() {
+    var video = document.getElementById('videoback');
+        video.src = "https://www.youtube.com/embed/9RHFFeQ2tu4?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W0LHTWG-UmQ";
+ 
+}
